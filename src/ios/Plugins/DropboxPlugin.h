271f15334e8e32194916934ac6a7db7d2bfaad40
Sergio Daniel Lozano Garcia <zheref@gmail.com>
2014-01-20 23:03:41 -0500

    First commit

---
+++ src/ios/Plugins/DropboxPlugin.h
@@ -0,0 +1,28 @@
+//
+//  DropboxPlugin.h
+//  FSTester
+//
+//  Created by SERGIO DANIEL LOZANO on 01/18/14.
+//
+//
+
+#import <Cordova/CDV.h>
+#import "DropboxPlugin.h"
+
+@interface DropboxPlugin : CDVPlugin
+
+- (void) link:(CDVInvokedUrlCommand*)command;
+
+- (void) checkLink:(CDVInvokedUrlCommand*)command;
+
+- (void) unlink:(CDVInvokedUrlCommand*)command;
+
+- (void)listFolder:(CDVInvokedUrlCommand*)command;
+
+- (void)addObserver:(CDVInvokedUrlCommand*)command;
+
+- (void)readData:(CDVInvokedUrlCommand*)command;
+
+- (void)readString:(CDVInvokedUrlCommand*)command;
+
+@end
